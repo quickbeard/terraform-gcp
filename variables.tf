6930a72b3b703397b8096cdf1d03e7bb6c97ea17
Nguyen <44449792+quickhair@users.noreply.github.com>
2022-12-13 13:46:05 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  default = "minh-sandbox"
+}
+
+variable "region_east" {
+  default = "us-east4"
+}
+
+variable "region_central" {
+  default = "us-central1"
+}
+
+variable "cluster" {
+  default = "testing-cluster"
+}
+
+variable "node_pool" {
+  default = "testing-node-pool"
+}
+
+variable "machine_type" {
+  default = "e2-medium"
+}
