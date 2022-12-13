f119a9bdc6d0940a0722d21dd0c348de8c4f37c6
Nguyen <44449792+quickhair@users.noreply.github.com>
2022-12-13 15:27:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
-/*
 resource "google_service_account" "default" {
   account_id = "service-account-id"
   display_name = "Service Account"
 }
-*/
 
 # Create a cluster
 resource "google_container_cluster" "my_cluster" {
